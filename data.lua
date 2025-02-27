d46cd48782a4bb2d8f9f0ef7f0c4f602d932906b
d.watanabe <daisuke.watanabe@citron-reiwa.com>
2025-02-27 21:02:45 +0900

    Added ideas and schedules.

--- data.lua
+++ data.lua
@@ -1,11 +1,5 @@
 -- 鉱石類
 require("prototypes.item")
 
--- Iron Formation 鉄鉱脈
-require("prototypes.iron-formation")
-
--- Copper Deposit 銅鉱床
-require("prototypes.copper-deposit")
-
 -- Bauxite Layer アルミニウム層
 require("prototypes.bauxite-layer")
